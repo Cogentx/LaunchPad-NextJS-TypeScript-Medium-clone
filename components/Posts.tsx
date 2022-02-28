fced1d69967cfbd272ad97ad4cafc6dbd5c9de33
Cogentx <code@cogentx.com>
2022-02-28 16:30:49 -0330

    Refactor Posts and Post into their own Components

---
+++ components/Posts.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { IPost } from '../typings';
+import { urlFor } from '../sanity';
+import Post from './Post';
+
+interface Props {
+  posts: [IPost];
+}
+
+function Posts({ posts }: Props) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
